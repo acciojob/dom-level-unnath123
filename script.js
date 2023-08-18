24b4b47720b323aa3b599e397747f4890c0afe5e
unnath123 <64188873+unnath123@users.noreply.github.com>
2023-08-18 15:36:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+let arr=document.getElementsByTagName("li");
+	for(let i=0;i<arr.length;i++){
+		if(arr[i].id=="level"){
+			alert("The elevel of the element is: "+ arr[i].innerHTML);
+		}
+	}
+	
+
+//console.log(arr);
+//console.log(arr.length);
